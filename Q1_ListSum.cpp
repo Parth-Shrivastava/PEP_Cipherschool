fcc734a657560640c0140b4de334221375f4510a
ParthShrivastava_22 <116020834+Parth-Shrivastava@users.noreply.github.com>
2025-01-17 23:02:13 +0530

    Create Q1_ListSum.cpp

---
+++ Q1_ListSum.cpp
@@ -0,0 +1,63 @@
+//Naive Approach:
+#include <iostream>
+    #include <list>
+    using namespace std;
+    
+    int main() {
+        list<int> integ;
+        int num;
+        char c;
+        while(true){
+            if(!(cin>>num)) break;
+            integ.push_back(num);
+        }
+        int sum=0;
+
+        for(int n:integ){
+            sum+=n;
+        }
+        cout<<sum;
+        return 0;
+    }
+
+//Recursive Approach
+#include <iostream>
+#include <list>
+using namespace std;
+
+ int Recursive_sum(list<int>:: iterator it, list<int>::iterator end){
+        if(it == end) return 0;
+        return *it+Recursive_sum(next(it),end);
+    }
+    
+    int main() {
+        list<int> integ;
+        int num;
+        char c;
+        while(true){
+            if(!(cin>>num)) break;
+            integ.push_back(num);
+        }
+        
+
+        cout<<Recursive_sum(integ.begin(),integ.end());
+        return 0;
+    }
+
+
+//Functional Approach
+
+ int main() {
+        list<int> integ;
+        int num;
+        char c;
+        while(true){
+            if(!(cin>>num)) break;
+            integ.push_back(num);
+        }
+        int sum=accumulate(integ.begin(),integ.end(),0);
+        
+
+        cout<<sum;
+        return 0;
+    }
